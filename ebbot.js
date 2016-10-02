53c413f160d6271e4757cdd8e22e72910b38eab2
Boris Karbanovich <bacsus_beacks@mail.ru>
2016-10-02 18:35:59 +0300

    [EBbot]: removed unnecessary commented out logs.

--- ebbot.js
+++ ebbot.js
@@ -14,18 +14,14 @@ class EBbot {
 		const router = telebot.router;
 		const routes = config.routes.right;
 
-		//let logs = '-------------LOGS-------------\n';
 		Object.keys(routes).forEach(command => {
 			const ctrlPath = './controllers/' + routes[command];
 			const Ctrl = require(ctrlPath);
 			const textCommand = new Telegram.TextCommand('/' + command);
-			//logs += ctrlPath + ': ' + (typeof Ctrl) + ' => /' + command + '\n';
 			router.when(textCommand, new Ctrl());
 		});
 
 		const OtherwiseCtrl = require('./controllers/' + config.routes.wrong);
-		//logs += './controllers/' + config.routes.wrong + ': ' + (typeof OtherwiseCtrl);
-		//setTimeout(console.log.bind(console, logs), 3000);
 		router.otherwise(new OtherwiseCtrl());
 	}
 }
